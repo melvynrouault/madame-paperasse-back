82605519c2310ec1f3b9b87aa32ed09d7174d508
melvynrouault <melvyn.rouault@gmail.com>
2023-07-05 23:59:27 +0200

    add dotenv library

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './services/mail/mailer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
